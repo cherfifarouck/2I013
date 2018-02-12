6a74ae9d9d448dda528ba775a78a84efc23b5c23
Yann Hoffmann <hyann98@gmail.com>
2018-02-12 16:14:43 +0100

    Changementstrat

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from FarouckYann import get_team
 
 #simu = Simulation(get_team(1),get_team(2))
 #simu = Simulation(get_team(1),get_team(3))
-#simu = Simulation(get_team(1),get_team(4))
-simu = Simulation(get_team(1),get_team(2))
+simu = Simulation(get_team(1),get_team(4))
+#simu = Simulation(get_team(1),get_team(2))
 #simu = Simulation(get_team(2),get_team(4))
 #simu = Simulation(get_team(3),get_team(4))
 
