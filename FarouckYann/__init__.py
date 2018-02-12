3e0fe1422f9b0cf35ca6b15e5067c4512021112c
Farouck Cherfi <farouck.cherfi@etu.upmc.fr>
2018-02-12 16:54:32 +0100

    changement

--- FarouckYann/__init__.py
+++ FarouckYann/__init__.py
@@ -2,28 +2,28 @@ from __future__ import absolute_import
 from .strat import *
 from soccersimulator import SoccerTeam
 
-def get_team (nb_team):
-	
-	if nb_team == 1:
-		myteam = SoccerTeam(name="Paris")
-		myteam.add("J1" ,FonceurEcole())
-	if nb_team == 2:
-		myteam = SoccerTeam(name="Lille")
-		myteam.add("J2", Marquage())
-	if nb_team == 3:
+def get_team (nb_players):
+	myteam = SoccerTeam(name="Real")
+	if nb_players == 1:
+		myteam.add("J1" ,Fonceur3())
+	if nb_players == 2:
 		
-		myteam = SoccerTeam(name="Real")
-
-		myteam.add("J1", Attaque())
-	if nb_team == 4:
-		myteam = SoccerTeam(name="Canard")
-		myteam.add("J1",LigneDroite())
-		
+		myteam.add("J1", Fonceur3())
+		myteam.add("J2" ,Fonceur3())
+	if nb_players == 3:
+		myteam.add("J1" ,Fonceur3())
+		myteam.add("J2", Fonceur3())
+		myteam.add("J3" ,Fonceur3())
+	if nb_players == 4:
+		myteam.add("J1" ,Fonceur3())	
+		myteam.add("J2", Fonceur3())
+		myteam.add("J3" ,Fonceur3())
+		myteam.add("J4" ,Fonceur3())
 	return myteam 
 
 
 def get_team_challenge(num):
-	myteam= SoccerTeam(name="Barca")
+	myteam= SoccerTeam(name="Real")
 	if num == 1:
 		myteam.add("Joueur Chal"+str(num),RandomStrategy())
 	return myteam
