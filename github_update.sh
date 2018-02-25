3f6e081b2c43bab15061cc8e5bc462fe9262740d
Yann Hoffmann <hyann98@gmail.com>
2018-02-25 15:15:54 +0100

    oui

--- github_update.sh
+++ github_update.sh
@@ -3,12 +3,15 @@ echo "Add nouveaux fichiers"
 git add *
 read -p "Entrer commentaire commit: " msg
 
-read -p "Veux tu sauvegarder? o/n" save
-if [$save == "o"]; then
-	echo "tu veux sauvegarder"
-fi
+read -p "Veux tu sauvegarder en local? o/n" save
 
 git commit -m "$msg" && git push
 
 printf "\n\n"
 git status
+
+if [$save == "o"]; then
+	echo "tu veux sauvegarder"
+else
+	echo "tu veux pas"
+fi
